962ef2d0df9af29ad571d6f284c1026de85e3df7
FoxelFox <storkner@gmail.com>
2014-10-29 11:53:21 +0100

    removed old project

--- TbpV2/scripts/tp01.groovy
+++
@@ -1,5 +0,0 @@
-package de.fh_zwickau.pti.tbpv2
-def tp = new TimePlanning(description: 'desc', tstamp: new Date(), timeBudgetPlan: 43, start: new Date())
-tp.save()
-def all = TimePlanning.getAll()
-all.size()
--- TbpV2/scripts/tp02.groovy
+++
@@ -1,8 +0,0 @@
-package de.fh_zwickau.pti.tbpv2
-def tp = new TimePlanning(description: 'xyz', timeBudgetPlan: 43, start: new Date(), tstamp: new Date())
-tp.save()
-for(err in tp.errors.fieldErrors) 
-    println "${err.class}: ${err.field} => ${err.rejectedValue}, code: ${err.code}"
-def all = TimePlanning.getAll()
-println all.id
-all.size()
