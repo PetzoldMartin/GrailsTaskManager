c98438a95f02521437f91c5dd0798038a59ec05b
Heliosana <markomania@gmx.net>
2014-11-12 11:56:23 +0100

    update beier branch

--- SPV/grails-app/conf/UrlMappings.groovy
+++ SPV/grails-app/conf/UrlMappings.groovy
@@ -1,37 +1,24 @@
 class UrlMappings {
 
-
-
 	static mappings = {
 		"/$controller/$action?/$id?(.$format)?"{ 
 			constraints { 
 				// apply constraints here
 				} 
 			}
-
+		
 		"/rest/"(controller: 'spv')
-		def resources = [
-			'students',
-			'examResults',
-			'lecturers',
-			'exams',
-			'subjects'
-		]
-		def actions = [
-			'show',
-			'save',
-			'update',
-			'delete'
-		]
-		// loop over all multi instance resources
-		for(resource in resources) {
-			"/rest/$resource/$id?"(controller: "${resource}Rest")
-			for(act in actions) {
-				"/rest/${resource}/${act}/$id?"(controller: "${resource}Rest", action: act)
-			}
+		"/rest/students"(resources: 'studentsRest') {
+			"/examResults"(resources: 'examResultsRest')
 		}
+		"/rest/lecturers"(resources: 'lecturersRest') {
+			"/exams"(resources:  'examsRest')
+		}
+		"/rest/subjects"(resources: 'subjectsRest')
 		"/rest/search/"(controller: 'searchrest', action: 'search')
+
 		"/"(view:"/index")
 		"500"(view:'/error')
+
 	}
 }
